5c567601f94439b641898457f67099b044d703a4
Proxigo35 <andrei.t35@outlook.com>
2026-05-14 17:51:38 +0100

    Added 14.3.

---
+++ 14/3/2/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+// Provide the definition for IntPair and the print() member function here
+struct IntPair {
+	int i1 {};
+	int i2 {};
+
+	void print() {
+		std::cout << "Pair(" << i1 << ", " << i2 << ")\n";
+	}
+
+	bool isEqual(const IntPair& pair) {
+		return (i1 == pair.i1 && i2 == pair.i2);
+	}
+
+};
+
+int main()
+{
+	IntPair p1 {1, 2};
+	IntPair p2 {3, 4};
+
+	std::cout << "p1: ";
+	p1.print();
+
+	std::cout << "p2: ";
+	p2.print();
+
+	std::cout << "p1 and p1 " << (p1.isEqual(p1) ? "are equal\n" : "are not equal\n");
+	std::cout << "p1 and p2 " << (p1.isEqual(p2) ? "are equal\n" : "are not equal\n");
+
+	return 0;
+}
