effeea6ed2ddeb350f398b1b5b19d993a43f8b97
Proxigo35 <andrei.t35@outlook.com>
2026-05-29 16:24:18 +0100

    Added 19.2

---
+++ 19/2/1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main() {
+
+	std::cout << "How many names would you like to enter? ";
+	std::size_t length {};
+	std::cin >> length;
+
+	auto* names {new std::string[length] {}};
+
+	for (std::size_t i {0}; i < length; ++i) {
+		std::cout << "Enter name #" << i + 1 << ": ";
+		std::getline(std::cin >> std::ws, names[i]);
+	}
+
+	std::sort(names, names  + length);
+	
+	std::cout << "\nHere is your sorted list: \n";
+	for (std::size_t i {0}; i < length; ++i){
+		std::cout << "Name #" << i + 1 << ": " << names[i] << '\n';
+	}
+
+	delete[] names;
+
+	return 0;
+}
