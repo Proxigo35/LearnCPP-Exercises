6ae4f646df55e3ffeba287ccef203b439e0e6299
Proxigo35 <andrei.t35@outlook.com>
2026-06-03 16:23:30 +0100

    Added 21.9.

---
+++ 21/9/1/main.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+struct StudentGrade {
+	std::string name {};
+	char grade {};
+};
+
+class GradeMap {
+	std::vector<StudentGrade> m_map {};
+
+public:
+	char& operator[](std::string_view name) {
+		auto found {
+			std::find_if(
+				m_map.begin(),
+				m_map.end(),
+				[name] (const auto& student) {
+					return student.name == name;
+				}
+			)
+		};
+
+		if(found != m_map.end()) return found->grade;
+		return m_map.emplace_back(std::string {name}).grade;
+	}
+};
+
+int main() {
+	GradeMap grades{};
+
+	grades["Joe"] = 'A';
+	grades["Frank"] = 'B';
+
+	std::cout << "Joe has a grade of " << grades["Joe"] << '\n';
+	std::cout << "Frank has a grade of " << grades["Frank"] << '\n';
+
+	return 0;
+}
