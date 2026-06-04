921e229dc964ddf052daf4f3c6f15d71538ee745
Proxigo35 <andrei.t35@outlook.com>
2026-06-04 11:00:24 +0100

    Added 21.10.

---
+++ 21/10/2/main.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <string_view>
+#include <iostream>
+
+class MyString {
+	std::string m_string {};
+
+public:
+	MyString(std::string_view s = {})
+		: m_string {s}
+	{}
+
+	friend std::ostream& operator<<(std::ostream& out, const MyString& s);
+
+	std::string_view operator()(std::size_t start, std::size_t end) {
+		return std::string_view{m_string}.substr(start, end);
+	}
+};
+
+std::ostream& operator<<(std::ostream& out, const MyString& s) {
+	out << s.m_string;
+	return out;
+}
+
+int main() {
+    MyString s {"Hello, world!"};
+    std::cout << s(7, 5) << '\n';
+
+    return 0;
+}
